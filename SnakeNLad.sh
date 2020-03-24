506eb17e556bffc8ec62d79332db7e014866dc10
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-24 06:04:33 -0700

    Use case 7

--- SnakeNLad.sh
+++ SnakeNLad.sh
@@ -1,29 +1,69 @@
 #!/bin/bash
 
 
-echo "Welcome to snake and Ladder program"
-
-position=0
-noOfRoll=0
-declare -a dic
-
-while [ $position -ne 100 ]
-do
-	diceRoll=$((RANDOM%6+1))
-	noOfRoll=$((noOfRoll+1))
-	r=$((RANDOM%2))
-
+playFunc () {
+	local playerChance=$1
+	local position=${player[$playerChance]}
+	local diceRoll=$((RANDOM%6+1))
+	local noOfRoll=$((noOfRoll+1))
+	r=$((RANDOM%3))
 	if [ $r -eq 0 ]
 	then
-		[[ "$((position+diceRoll))" -gt 100 ]] && continue || position=$((position+diceRoll))
-		echo $position
+		if [[ "$((position+diceRoll))" -le 100 ]] 
+		then
+			position=$((position+diceRoll))
+			player[$playerChance]=$position
+			echo "Player" $playerChance ":"$position
+		fi
 	elif [ $r -eq 1 ]
 	then
-		[[ "$((position-diceRoll))" -lt "0" ]] && continue || position=$((position-diceRoll))
-		echo $position
+		if [[ "$((position-diceRoll))" -ge 0 ]]
+		then
+		 	position=$((position-diceRoll))
+			player[$playerChance]=$position
+			echo "Player" $playerChance ":"$position
+		fi
 	else
- 		echo $position
+		echo "Player" $playerChance ":"$position
+	fi
+	if [ $diceRoll -eq 6 ]
+	then
+		playFunc $playerChance
+	fi
+}
+
+echo "Welcome to snake and Ladder program"
+
+declare -A player
+player[1]=0
+player[2]=0
+noOfRoll=0
+playerFlag=1
+while true
+do
+	if [ $playerFlag -eq 1 ] 
+	then
+		playFunc 1
+		playerFlag=2
+		if [ ${player[1]} -eq 100 ]
+		then
+			echo Player 1 won
+			break
+		else
+			continue
+		fi
+	elif [ $playerFlag -eq 2 ]
+	then
+		playFunc 2
+		playerFlag=1
+		if [ ${player[2]} -eq 100 ]
+		then
+			echo Player 2 won
+			break
+		else
+			continue
+		fi
+
 	fi
 done
-
 echo Total No of dice Roll : $noOfRoll
