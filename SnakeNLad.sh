ab90ebad2599da680c92caf6656f586bfd950519
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-24 04:01:02 -0700

    Use case 3

--- SnakeNLad.sh
+++ SnakeNLad.sh
@@ -7,3 +7,15 @@ position=0
 
 
 dieRoll=$((RANDOM%6+1))
+
+r=$((RANDOM%2))
+
+if [ $r -eq 0 ]
+then
+	echo "Ladder, moving player ahead by" $dieRoll "Position"
+elif [ $r -eq 1 ]
+then
+	echo "Snake, movin player behind by "$dieRoll" Position"
+else
+ 	echo "you Stay there"
+fi
