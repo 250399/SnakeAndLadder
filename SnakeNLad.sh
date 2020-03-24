ae9e6c75b14079b13fc04e1eff885521cc350a00
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-24 04:14:01 -0700

    use case 4

--- SnakeNLad.sh
+++ SnakeNLad.sh
@@ -5,17 +5,21 @@ echo "Welcome to snake and Ladder program"
 
 position=0
 
+while [ $position -lt 100 ]
+do
+	diceRoll=$((RANDOM%6+1))
 
-dieRoll=$((RANDOM%6+1))
+	r=$((RANDOM%2))
 
-r=$((RANDOM%2))
-
-if [ $r -eq 0 ]
-then
-	echo "Ladder, moving player ahead by" $dieRoll "Position"
-elif [ $r -eq 1 ]
-then
-	echo "Snake, movin player behind by "$dieRoll" Position"
-else
- 	echo "you Stay there"
-fi
+	if [ $r -eq 0 ]
+	then
+		position=$((position+diceRoll))
+		echo $position
+	elif [ $r -eq 1 ]
+	then
+		[[ "$((position-diceRoll))" -lt "0" ]] && continue || position=$((position-diceRoll))
+		echo $position
+	else
+ 		echo $position
+	fi
+done
